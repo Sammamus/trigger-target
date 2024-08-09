fb2dece0b253825603f0810cb7991b2c8c2a678a
Darian Raffle <darian.raffle@gmail.com>
2024-08-09 10:45:43 -0500

    Initial Push

---
+++ helloWorld.py
@@ -0,0 +1,26 @@
+import sys
+import time
+
+# Global
+sleep_timer = 60
+
+
+def sleeper():
+    print("Sleeping")
+    time.sleep(sleep_timer)
+    print("Done")
+
+
+def main(name):
+    try:
+        print(f"Hello {name}!")
+        time.sleep(sleep_timer)
+        exit(0)
+    except Exception as e:
+        print(e)
+        exit(1)
+
+
+if __name__ == '__main__':
+    arg = sys.argv[1]
+    main(arg)
