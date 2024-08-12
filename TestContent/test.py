e713e6e9ba3c16f09031aa5cb3ed5507ecc9fa4c
Darian Raffle <darian.raffle@gmail.com>
2024-08-12 08:00:27 -0500

    testing against workflow_dispatch

--- TestContent/test.py
+++ TestContent/test.py
@@ -2,12 +2,13 @@ import os
 import urllib3
 
 # Globals
-http = urllib3.PoolManager()
+# http = urllib3.PoolManager()
 token = os.environ['GH_PAT']
 owner = ""
 repo = ""
 branch = ""
 event_type = ""
+workflows = []
 
 
 def set_owner():
@@ -52,7 +53,7 @@ def set_branch():
 def set_event_type():
     global event_type
 
-    valid_types = ["trigger-workflow", "trigger-workflow01"]
+    valid_types = ["trigger-workflow", "completely-different-name"]
 
     while(True):
         print("\n--- Valid Event Types ---")
@@ -69,7 +70,7 @@ def set_event_type():
                 event_type = "trigger-workflow"
                 break
             case 1:
-                event_type = "trigger-workflow01"
+                event_type = "completely-different-name"
                 break
             case _:
                 print("Invalid input")
@@ -83,28 +84,87 @@ def get_headers():
     }
 
 
-def get_data(event_type, arguement):
+def get_data(argument):
     return {
         "event_type": event_type,
         "client_payload": {
             "target_branch": branch,
             "target_repository": f"{owner}/{repo}",
-            "target_argument": arguement
+            "target_argument": argument
         }
     }
 
 
+def create_client():
+    return urllib3.PoolManager()
+
+
+def set_workflows():
+    global workflows
+
+    http = create_client()
+
+    resp = http.request(
+        "GET",
+        f"https://api.github.com/repos/{owner}/{repo}/actions/workflows",
+        headers=get_headers()
+    )
+    if resp.status == 200:
+        print("Successfully fetched workflows")
+        print(resp.data)
+
+        response = resp.json()
+
+        for w in response['workflows']:
+            workflows.append({w['name']: w['id']})
+
+    elif resp.status > 399:
+        print(resp.data)
+        raise Exception(resp.status)
+    else:
+        print("Failed to fetch workflows")
+        print(resp.status)
+        print(resp.data)
+
+
+def select_workflow():
+    output = ""
+
+    print("\n--- Workflows ---")
+    count = 0
+    for w in workflows:
+        print(f"\t({count}) {w}")
+        count += 1
+
+    while(True):
+        workflow = int(input("\nPlease select a workflow: "))
+
+        match workflow:
+            case 0:
+                output = workflows[0]['id']
+                break
+            case 1:
+                output = workflows[1]['id']
+                break
+            case _:
+                print("Invalid input")
+
+    return output
+
+
 def call_dispatch():
+    http = create_client()
+    workflow_id = select_workflow()
     set_branch()
     set_event_type()
 
-    arguement = input("Enter the argument you wish to send to GitHub: ")
+    argument = input("Enter the argument you wish to send to GitHub: ")
 
     resp = http.request(
         "POST",
-        f"https://api.github.com/repos/{owner}/{repo}/dispatches",
+        f"https://api.github.com/repos/{owner}/{repo}/actions/workflows/{workflow_id}/dispatches",
         headers=get_headers(),
-        json=get_data("trigger-workflow", arguement),
+        json=get_data(argument),
     )
 
     if resp.status == 204:
@@ -125,6 +185,7 @@ def main():
         print("\n--- Welcome to Github Trigger Workflow Testing ---\n")
         set_owner()
         set_repo()
+        set_workflows()
 
         quit = ["q", "quit", "n", "no"]
 
@@ -132,7 +193,6 @@ def main():
             call_dispatch()
 
             user_response = input ("\nWould you like to continue? [y/n]: ")
-
 
         print("Thank you for using Github Trigger Workflow Testing!")
         exit(0)
