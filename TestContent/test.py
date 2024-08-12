258567d7bb74a2bd162a49f36c8ad27e4bd7accc
Darian Raffle <darian.raffle@gmail.com>
2024-08-12 12:58:53 -0500

    testing against workflow_dispatch

--- TestContent/test.py
+++ TestContent/test.py
@@ -1,7 +1,10 @@
-import json
 import os
 import time
+import uuid
+
 import urllib3
+
+from datetime import datetime, timezone
 
 # Globals
 # http = urllib3.PoolManager()
@@ -11,6 +14,10 @@ repo = ""
 branch = ""
 event_type = ""
 workflows = []
+query_date = ""
+workflow_uuid = ""
+workflow_run_id = ""
+workflow_found = False
 
 
 def set_owner():
@@ -156,20 +163,31 @@ def select_workflow():
 
 
 def list_workflow_runs():
+    global workflow_run_id, workflow_found
+
     http = create_client()
 
     resp = http.request(
         "GET",
-        f"https://api.github.com/repos/{owner}/{repo}/actions/runs?status=pending&status=waiting&status=requested",
+        f"https://api.github.com/repos/{owner}/{repo}/actions/runs?created=>{query_date}",
         headers=get_headers()
     )
     if resp.status == 200:
         print("Successfully fetched workflow runs")
-        print(resp.data)
+        # print(resp.data)
 
         response = resp.json()
 
-        print(json.dumps(response, indent=2))
+        for run in response['workflow_runs']:
+            if run['name'] == workflow_uuid:
+                print("Workflow run found!")
+                workflow_found = True
+                workflow_run_id = run['id']
+
+        if not workflow_found:
+            print("Workflow run not found!")
+
+        # print(json.dumps(response, indent=2))
 
         # for w in response['workflows']:
         #     file = w['path'].split('/')[-1]
@@ -184,19 +202,81 @@ def list_workflow_runs():
         print(resp.data)
 
 
+def monitor_workflow_runs():
+    if workflow_found:
+
+        happy_endings = [
+            "completed",
+            "success"
+        ]
+
+        bad_endings = [
+            "cancelled",
+            "failure"
+        ]
+
+        count = 0
+        status = ""
+
+        while count <= 5:
+
+            if count == 5:
+                status = f"Monitor Timeout after {count} checks"
+                break
+
+            http = create_client()
+            resp = http.request(
+                "GET",
+                f"https://api.github.com/repos/{owner}/{repo}/actions/runs/{workflow_run_id}",
+                headers=get_headers()
+            )
+
+            if resp.status == 200:
+                response = resp.json()
+
+                if response['status'] in happy_endings:
+                    status = response['status']
+                    break
+
+                if response['status'] in bad_endings:
+                    status = response['status']
+                    break
+
+            elif resp.status > 399:
+                print(resp.data)
+                raise Exception(resp.status)
+            else:
+                status = "Failed to fetch workflows"
+                # print(resp.status)
+                # print(resp.data)
+                break
+
+            count += 1
+            time.sleep(30)
+
+        print("\n--- Workflow Status ---")
+        print(f"\t- Workflow Run ID: {workflow_run_id}")
+        print(f"\t- Workflow Name: {workflow_uuid}")
+        print(f"\t- Status: {status}")
+
+
 def call_dispatch():
+    global query_date, workflow_uuid
+
+    query_date = datetime.now(timezone.utc).strftime('%Y-%m-%dT%H:%M:%SZ')
+
     http = create_client()
     workflow_id = select_workflow()
     set_branch()
     # set_event_type()
 
-    argument = input("Enter the argument you wish to send to GitHub: ")
+    workflow_uuid = f"test-{uuid.uuid4().hex}"
 
     resp = http.request(
         "POST",
         f"https://api.github.com/repos/{owner}/{repo}/actions/workflows/{workflow_id}/dispatches",
         headers=get_headers(),
-        json=get_data(argument),
+        json=get_data(workflow_uuid),
     )
 
     if resp.status == 204:
@@ -225,6 +305,7 @@ def main():
             call_dispatch()
             time.sleep(30)
             list_workflow_runs()
+            monitor_workflow_runs()
 
             user_response = input ("\nWould you like to continue? [y/n]: ")
 
